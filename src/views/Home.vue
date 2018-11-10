8c111a8d3a051b3dd0845397fb96444d49df9e61
NoriyoshiSatou <noriyoshi.sato@rootage.co.jp>
2018-11-10 12:10:24 +0900

    あのアイコンの削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/siratama_debug_icon.png">
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msgs="Welcome to ようこそ！俺の庭へ！"/>
     <Second/>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 });
 </script>
